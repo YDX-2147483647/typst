a658ebfdc90a6d28c0fe2d1682703dc11c3502fe
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-09-05 00:18:31 +0800

    ci(netlify): Use the latest typst-jp/docs and simplify the build script
    Thanks to https://github.com/typst-jp/docs/issues/233.

--- netlify-build-docs.sh
+++ netlify-build-docs.sh
@@ -6,20 +6,20 @@ set -euxo pipefail
 curl -sS https://webi.sh/sd | sh && source ~/.config/envman/PATH.env
 curl https://mise.run | sh
 
-# 2. Clone typst-jp.github.io
+# 2. Clone typst-jp/docs
 
-# Tested against https://github.com/typst-jp/typst-jp.github.io/commit/f588f0d53dbb39225739fa7faeecb33d9b4b055b
-git clone --depth 1 --no-checkout --filter=tree:0 https://github.com/typst-jp/typst-jp.github.io ../jp
+# Tested against https://github.com/typst-jp/docs/commit/529c33dec006b20729795ca64e3079e18ae4e1d9
+git clone --depth 1 --no-checkout --filter=tree:0 https://github.com/typst-jp/docs ../jp
 cd ../jp
 git sparse-checkout init
 git sparse-checkout set website/ tsconfig.json package.json bun.lockb .mise.toml
 git switch main
 cd -
 
-# 3. Move files from typst-jp
+# 3. Move files from typst-jp/docs
 
 cat << EOF >> .gitignore
-# From typst-jp
+# From typst-jp/docs
 /website/
 /tsconfig.json
 /package.json
@@ -27,8 +27,8 @@ cat << EOF >> .gitignore
 /.mise.toml
 
 # Generated
-/assets/docs/
-/assets/docs.json
+/assets/
+/docs.json
 EOF
 
 # This `cp -r` cannot be replaced with `ln -s`, because there will be a symlink created in website/public
@@ -39,24 +39,29 @@ cp ../jp/.mise.toml .
 # The rust edition has been bumped to 2024 in https://github.com/typst/typst/pull/6637
 sd --fixed-strings \
     'rust = "1.83.0"' \
-    'rust = "1.88.0"' \
+    'rust = "1.89.0"' \
     .mise.toml
 
-# Use canonical typst-docs for `mise run generate-docs`
+# Change the base
 sd --fixed-strings \
-    'run = "cargo test --package typst-docs --lib -- tests::test_docs --exact --nocapture"' \
-    'run = "cargo run --package typst-docs -- --assets-dir assets/docs --out-file assets/docs.json --base /docs/"' \
+    'run = "cargo run --package typst-docs -- --assets-dir assets --out-file docs.json --base /docs/"' \
+    'run = "cargo run --package typst-docs -- --assets-dir assets --out-file docs.json --base /"' \
     .mise.toml
-mkdir -p website/public/docs
 sd --fixed-strings \
-    'const publicAssetsDocsPath = resolve(__dirname, "./public/assets/docs/");' \
-    'const publicAssetsDocsPath = resolve(__dirname, "./public/docs/assets/");' \
-    website/vite.config.ts
+    'export const basePath: "/" | `/${string}/` = "/docs/";' \
+    'export const basePath: "/" | `/${string}/` = "/";' \
+    website/src/metadata.ts
+
+# Switch to English
+sd --fixed-strings \
+    'export { translation, Translation } from "./ja-JP";' \
+    'export { translation, Translation } from "./en-US";' \
+    website/src/translation/index.tsx
 
 # Replace the typst version with the commit hash
 sd --fixed-strings \
     '"version": "0.13.1"' \
-    "\"version\": \"dev.$(git rev-parse --short HEAD)\"" \
+    "\"version\": \"dev.$(git log -1 --format=%cs)\"" \
     website/package.json
 
 # 4. Build
@@ -64,9 +69,6 @@ sd --fixed-strings \
 mise trust
 mise install
 mise run generate
-
-# For debugging:
-cp assets/docs.json website/dist/
 
 # For dev: Run `mise run dev`
 # For deploy: Upload website/dist
